a45a1e2ad77af18ed2a7f8b392e0b13cd7161af5
eldarlrd <eldarlrd@protonmail.com>
2023-01-13 23:48:28 +0400

    feat: Initial logic

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
-$(document).ready(function() {
-
+// Executor
+$(document).ready(() => {
+  $(this).keydown(key => {
+    key.keyCode !== 82
+    ? handleDrum(key.keyCode)
+    : handleSwitch(key.keyCode);
+  });
+  $(".drum-pad").click(() => {
+    handleDrum();
+  });
 });
+// Drum Player
+const handleDrum = num => {
+  const btn = $(keys[num].id);
+  btn.addClass("drumActive");
+  setTimeout(() => {
+    btn.removeClass("drumActive");
+  }, 400);
+};
+// Instrument Switch
+const handleSwitch = r => {
+
+};
+// Keys Object
+const keys = {
+  81: {
+    "id": "#sound-1"
+  },
+  87: {
+    "id": "#sound-2"
+  },
+  69: {
+    "id": "#sound-3"
+  },
+  65: {
+    "id": "#sound-4"
+  },
+  83: {
+    "id": "#sound-5"
+  },
+  68: {
+    "id": "#sound-6"
+  },
+  90: {
+    "id": "#sound-7"
+  },
+  88: {
+    "id": "#sound-8"
+  },
+  67: {
+    "id": "#sound-9"
+  },
+  82: {
+
+  }
+};
+/* Keycodes
+  Q 81, W 87, E 69
+  A 65, S 83, D 68,
+  Z 90, X 88, C 67, R 82
+*/
