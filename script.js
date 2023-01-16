7641b75c8a6a116d05a59a19333568c5cea2e589
eldarlrd <eldarlrd@protonmail.com>
2023-01-16 14:12:51 +0400

    feat: Session storage

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ $(document).ready(() => {
   $(".drum-pad").click(function() {
     drumStyle($(this));
   });
-  $("#volume-bar").change(volControl);
+  $("#volume-bar").on("input", volControl);
   volCheck();
 });
 // Drum Styler (Keyboard)
 const keyStyle = num => {
-  const btn = $(keys[num].id);
+  const btn = $(keys[num]);
   btn.addClass("drumActive");
   setTimeout(() => {
     btn.removeClass("drumActive");
@@ -35,63 +35,34 @@ const drumStyle = str => {
 };
 // Volume Controls
 const volControl = () => {
-  const i = $("#volume-bar").val();
-  switch (i) {
-    case "0":
-      $(".clip").prop("volume", 0);
-      break;
-    case "1":
-      $(".clip").prop("volume", .25);
-      break;
-    case "2":
-      $(".clip").prop("volume", .5);
-      break;
-    case "3":
-      $(".clip").prop("volume", .75);
-      break;
-    case "4":
-      $(".clip").prop("volume", 1);
-  }
+  const vol = $("#volume-bar").val() / 4;
+  sessionStorage.setItem("volSession", vol);
   volCheck();
 };
 // Volume Checker
 const volCheck = () => {
-  $("#volume-bar").val() != 0
+  const volSaved = sessionStorage.getItem("volSession");
+  volSaved === null
+    ? $(".clip").prop("volume", .5)
+    : $(".clip").prop("volume", volSaved);
+  volSaved != 0
     ? $("#volume-icon").prop("src", volIcon.high)
-    : $("#volume-icon").prop("src", volIcon.low);
+    : $("#volume-icon").prop("src", volIcon.mute);
 };
 // Keycodes
 const keys = {
-  81: {
-    "id": "#sound-1"
-  },
-  87: {
-    "id": "#sound-2"
-  },
-  69: {
-    "id": "#sound-3"
-  },
-  65: {
-    "id": "#sound-4"
-  },
-  83: {
-    "id": "#sound-5"
-  },
-  68: {
-    "id": "#sound-6"
-  },
-  90: {
-    "id": "#sound-7"
-  },
-  88: {
-    "id": "#sound-8"
-  },
-  67: {
-    "id": "#sound-9"
-  }
+  81: "#sound-1",
+  87: "#sound-2",
+  69: "#sound-3",
+  65: "#sound-4",
+  83: "#sound-5",
+  68: "#sound-6",
+  90: "#sound-7",
+  88: "#sound-8",
+  67: "#sound-9"
 };
 // Volume Icons
 const volIcon = {
   "high": "assets/icons/volume-high-solid.svg",
-  "low": "assets/icons/volume-xmark-solid.svg"
+  "mute": "assets/icons/volume-xmark-solid.svg"
 };
